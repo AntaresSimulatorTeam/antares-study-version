65c6d2855a81dd282e4bfbd79cc06b10f475d536
Anis SMAIL <anis.smail_externe@rte-france.com>
2025-04-23 17:08:48 +0200

    reformat

--- src/antares/study/version/__pyinstaller/hook-antares.study.version.py
+++ src/antares/study/version/__pyinstaller/hook-antares.study.version.py
@@ -1,5 +1,5 @@
 from PyInstaller.utils.hooks import collect_data_files
 
-hiddenimports = ['antares.study.version.create_app.resources']
+hiddenimports = ["antares.study.version.create_app.resources"]
 
-datas = collect_data_files('antares.study.version.create_app.resources')
+datas = collect_data_files("antares.study.version.create_app.resources")
