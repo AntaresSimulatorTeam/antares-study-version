450776837f7d0f0dcb293faf0863934266642026
Anis SMAIL <anis.smail_externe@rte-france.com>
2025-04-25 15:32:47 +0200

    remove tests config

--- src/antares/study/version/__pyinstaller/__init__.py
+++ src/antares/study/version/__pyinstaller/__init__.py
@@ -10,17 +10,3 @@ def get_hook_dirs():
     """
 
     return [os.path.dirname(__file__)]
-
-
-def get_PyInstaller_tests():
-    """
-    Retrieve the directory path where pyinstaller will look for test hooks.
-
-    These paths are then passed to pytest for test discovery. This allows both
-    testing by this package and by PyInstaller.
-
-    Returns:
-        list: A list with the directory path of the test hooks.
-    """
-
-    return [os.path.dirname(__file__)]
