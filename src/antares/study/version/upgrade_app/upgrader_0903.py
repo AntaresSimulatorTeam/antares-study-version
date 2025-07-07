9a3144858d7d8620ee8b22879a7710ccb391c251
Vincent Payet <vincent.payet@rte-france.com>
2025-07-07 15:58:08 +0200

    Thematic trimming for thermal and renew

--- src/antares/study/version/upgrade_app/upgrader_0903.py
+++ src/antares/study/version/upgrade_app/upgrader_0903.py
@@ -19,6 +19,60 @@ class UpgradeTo0903(UpgradeMethod):
     old = StudyVersion(9, 2)
     new = StudyVersion(9, 3)
 
+def _upgrade_thematic_trimming(data: GeneralData) -> None:
+    def _get_possible_thermal_variables() -> t.Set[str]:
+        groups = ["nuclear", "lignite", "coal", "battery", "gas", "oil", "mix. fuel", "misc. dtg", "misc. dtg 2", "misc. dtg 3", "misc. dtg 4"]
+        return groups
+
+    def _get_possible_renewable_variables() -> t.Set[str]:
+        groups = ["wind offshore", "wind onshore", "solar concrt.", "solar pv", "solar rooft", "renw. 1", "renw. 2", "renw. 3", "renw. 4"]
+        return groups
+
+    variables_selection = data["variables selection"]
+    possible_variables = _get_possible_thermal_variables()
+    d: t.Dict[str, t.Dict[str, t.List[str]]] = {}
+    for sign in ["+", "-"]:
+        select_var = f"select_var {sign}"
+        d[select_var] = {"keep": [], "remove": []}
+        # The 'remove' list gathers all fields that should not be kept after the upgrade.
+        # It applies to any field listed by the `_get_possible_variables` methods.
+        # The 'keep' list gathers all fields that have nothing to do with the upgrade and therefore should be kept.
+        # We check these fields for enabled and disabled variables (symbolized by +/-) as we can have both.
+        # In the end, we remove all legacy fields and replace them either by "DISPATCH. GEN." or "RENEWABLE GEN.".
+        for var in variables_selection.get(select_var, []):
+            key = "remove" if var.lower() in possible_variables else "keep"
+            d[select_var][key].append(var)
+
+    if d["select_var +"]["remove"] and d["select_var -"]["remove"]:
+        raise UnexpectedThematicTrimmingFieldsError(d["select_var +"]["remove"], d["select_var -"]["remove"])
+    for sign in ["+", "-"]:
+        select_var = f"select_var {sign}"
+        if d[select_var]["keep"]:
+            d[select_var]["keep"].append("DISPATCH. GEN.")
+            variables_selection[select_var] = d[select_var]["keep"]
+
+    possible_variables = _get_possible_renewable_variables()
+    d: t.Dict[str, t.Dict[str, t.List[str]]] = {}
+    for sign in ["+", "-"]:
+        select_var = f"select_var {sign}"
+        d[select_var] = {"keep": [], "remove": []}
+        # The 'remove' list gathers all fields that should not be kept after the upgrade.
+        # It applies to any field listed by the `_get_possible_variables` methods.
+        # The 'keep' list gathers all fields that have nothing to do with the upgrade and therefore should be kept.
+        # We check these fields for enabled and disabled variables (symbolized by +/-) as we can have both.
+        # In the end, we remove all legacy fields and replace them either by "DISPATCH. GEN." or "RENEWABLE GEN.".
+        for var in variables_selection.get(select_var, []):
+            key = "remove" if var.lower() in possible_variables else "keep"
+            d[select_var][key].append(var)
+
+    if d["select_var +"]["remove"] and d["select_var -"]["remove"]:
+        raise UnexpectedThematicTrimmingFieldsError(d["select_var +"]["remove"], d["select_var -"]["remove"])
+    for sign in ["+", "-"]:
+        select_var = f"select_var {sign}"
+        if d[select_var]["keep"]:
+            d[select_var]["keep"].append("RENEWABLE GEN.")
+            variables_selection[select_var] = d[select_var]["keep"]
+
     @staticmethod
     def _upgrade_general_data(study_dir: Path) -> None:
         data = GeneralData.from_ini_file(study_dir)
@@ -29,6 +83,9 @@ class UpgradeTo0903(UpgradeMethod):
         general.pop("refreshintervalwind", None)
         general.pop("refreshintervalthermal", None)
         general.pop("refreshintervalsolar", None)
+
+        if "variables selection" in data:
+            _upgrade_thematic_trimming(data)
 
         data.to_ini_file(study_dir)
 
