bbd5fc1d9e2eb546a6d0aa5a3bdac795b3232e1e
Theo Pascoli <theo.pascoli@rte-france.com>
2025-07-07 09:40:02 +0200

    chore(release): v1.0.18

--- src/antares/study/version/__about__.py
+++ src/antares/study/version/__about__.py
@@ -4,7 +4,7 @@ Antares Study (and Solver) version models.
 
 # Standard project metadata
 
-__version__ = "1.0.17"
+__version__ = "1.0.18"
 __author__ = "RTE, Antares Web Team"
-__date__ = "2025-05-14"
+__date__ = "2025-07-07"
 __credits__ = "© Réseau de Transport de l’Électricité (RTE)"
